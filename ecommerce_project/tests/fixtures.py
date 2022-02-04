99d46ddab124b13a0214966373b3f583cc4e0eb0
Eric <nextstep@ericdee.me>
2022-02-03 18:32:18 -0700

    1:22:45

--- ecommerce_project/tests/fixtures.py
+++ ecommerce_project/tests/fixtures.py
@@ -1,4 +1,6 @@
 import pytest
+from django.core.management import call_command
+
 
 @pytest.fixture
 def create_admin_user(django_user_model):
@@ -6,3 +8,11 @@ def create_admin_user(django_user_model):
     Return admin user
     '''
     return django_user_model.objects.create_superuser('superuser', '', 'xyz')
+
+@pytest.fixture(scope='session')
+def db_fixture_setup(django_db_setup, django_db_blocker):
+    '''
+    Unlocks database and uses fixtures as inserts
+    '''
+    with django_db_blocker.unblock():
+        call_command("loaddata", "db_admin_fixture.json")
